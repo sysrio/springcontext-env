d5b6bf74bfa0d84761406e6cdb63826e3f0b828a
Calvince Otieno <191495738+sysrio@users.noreply.github.com>
2024-12-17 10:30:18 -0500

    feat: Add EnvContextLoaderException class to handle exceptions
    Added a custom exception `EnvContextLoaderException` that
    extends `RuntimeException`. This exception is thrown when
    there is an error loading environment variables from dotenv
    files in a Spring application.
    
    Details:
    
    - Provides constructors for detailed error messages and
      underlying causes.
    
    - Enhances error handling during application context
      initialization.
    
    - Improves debugging by giving clearer insights into
      environment setup issues.

---
+++ src/main/java/io/sysr/springcontext/env/exception/EnvContextLoaderException.java
@@ -0,0 +1,58 @@
+package io.sysr.springcontext.env.exception;
+
+import org.springframework.lang.NonNull;
+
+/**
+ * Exception thrown when there is an error loading the environment variables
+ * from a dotenv file in a Spring application. This exception extends
+ * {@code RuntimeException}, indicating it is a unchecked exception that can
+ * occur during the normal operation of the application, particularly during
+ * environment setup or context loading.
+ *
+ * <p>
+ * Using this exception helps to pinpoint issues related to the application
+ * context
+ * not being properly configured due to missing or misconfigured environment
+ * variables.
+ * </p>
+ *
+ * <p>
+ * Example usage:
+ * </p>
+ * 
+ * <pre>
+ * try {
+ *     // Code that loads environment variables
+ * } catch (IOException e) {
+ *     throw new EnvContextLoaderException("Failed to load environment variables", e);
+ * }
+ * </pre>
+ *
+ * @author Calvince Otieno
+ * @version 1.0
+ * @since 2024
+ */
+public class EnvContextLoaderException extends RuntimeException {
+    /**
+     * Constructs a new EnvContextLoaderException with the specified detail message.
+     *
+     * @param message the detail message, saved for later retrieval by the
+     *                {@link #getMessage()} method.
+     */
+    public EnvContextLoaderException(@NonNull String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new EnvContextLoaderException with the specified detail message
+     * and cause.
+     *
+     * @param message the detail message explaining the reason for the exception,
+     *                saved for later retrieval by the {@link #getMessage()} method.
+     * @param cause   the underlying cause of the exception,
+     *                saved for later retrieval by the {@link #getCause()} method.
+     */
+    public EnvContextLoaderException(@NonNull String message, Throwable cause) {
+        super(message, cause);
+    }
+}
